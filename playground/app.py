9c9ad5892501bcbf1271f178a4b70fdaf5a2720d
Maureen <maureen.sledge16@stjohns.edu>
2019-02-13 15:11:21 -0500

    changes

--- playground/app.py
+++ playground/app.py
@@ -14,9 +14,9 @@ def wel(name):
     return render_template("welcome.html", name=name)
 
 
-@app.route("/roster/<int:grade_view>")
+@app.route("/roster/<string:grade_view>")
 def roster(grade_view):
-    class_roster = [("Hermione","A","Sophomore"), ("Harry","B","Sophomore"), ("Ron", "C", "Sophomore"), ("Ginny","B-","Freshman"), ("Neville","D","Sophomore"), ("Luna","A", "Freshman"), ("Percy","A", "Senior"), ("Fred","C", "Junior"), ("George","C+", "Junior")]
+    class_roster = [("Hermione", "A+", "Sophomore"), ("Harry", "B", "Sophomore"), ("Ron", "C", "Sophomore"), ("Ginny", "B-", "Freshman"), ("Neville", "D", "Sophomore"), ("Luna", "A", "Freshman"), ("Percy", "A", "Senior"), ("Fred", "C", "Junior"), ("George", "C+", "Junior")]
     return render_template("roster.html", class_roster=class_roster, grade_view=grade_view)
 
 
